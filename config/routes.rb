f93f3c3008a23b28ef4c53da506f954e2bd35f78
Seiya-wasabi <se.sato614@gmail.com>
2021-05-01 07:53:32 +0000

    普通に手間取った

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
   devise_for :users
-  root :to => "homes#top"
-  get '/about' => 'homes#about'
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+end
