fecca7f2dbdf71765f484ce0f53b887771af981d
EC2 Default User <ec2-user@ip-172-31-3-3.ec2.internal>
2021-05-02 06:53:45 +0000

    コメント用のコントローラーとモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
+  resources :book_comments, only: [:create, :destroy]
   resources :books
 end
