89f7399be2a107ba439e61afc915c00bb8735e37
EC2 Default User <ec2-user@ip-172-31-3-3.ec2.internal>
2021-05-02 07:06:34 +0000

    1:Nの関係を作った

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
 	# has_many :user
 	belongs_to :user
+	has_many :book_comments, dependent: :destroy
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
