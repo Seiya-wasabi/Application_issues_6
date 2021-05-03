081fc65d7b1603d31e41faebf47b9e0b750db363
EC2 Default User <ec2-user@ip-172-31-3-3.ec2.internal>
2021-05-03 12:27:03 +0000

    コントローラーが上手くできん

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,7 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
+    book_comment = Book.find(params[:id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to books_path
   end
 
   def destroy
+  end
+  
+    private
+
+  def post_comment_params
+    params.require(:book_comment).permit(:comment)
   end
 end
