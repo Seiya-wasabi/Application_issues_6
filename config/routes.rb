7b960b5a2f94643754a9fb328c4077d74a37def0
EC2 Default User <ec2-user@ip-172-31-3-3.ec2.internal>
2021-05-04 13:34:15 +0000

    一旦全部終了したけどフォローボタンが表示されへん

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
   get 'book_comments/create'
   get 'book_comments/destroy'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
+  resources :relationships, only: [:create, :destroy]
   
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
