c2ad7d88aaf44e54ee12be49f966605f3b229c61
EC2 Default User <ec2-user@ip-172-31-3-3.ec2.internal>
2021-05-06 13:35:59 +0000

    コントローラーとアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'search/search'
   get 'book_comments/create'
   get 'book_comments/destroy'
   devise_for :users
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   get "relationship/:id" => "relationships#index", as: "relationships"
   get "relationship/:id" => "relationships#followers", as: "followed"
+  get '/search' => 'search#search'
   resources :users,only: [:show,:index,:edit,:update]
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
