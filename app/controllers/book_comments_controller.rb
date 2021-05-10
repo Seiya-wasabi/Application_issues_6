360ac44c1dba2baf4ef51e00e4d16023d0f4484d
EC2 Default User <ec2-user@ip-172-31-3-3.ec2.internal>
2021-05-10 09:35:12 +0000

    非同期処理完成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,11 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @user = @book.user
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(comment.book_id)
+    # redirect_to book_path(comment.book_id)
     # post_image = PostImage.find(params[:post_image_id])
     # comment = current_user.post_comments.new(post_comment_params)
     # comment.post_image_id = post_image.id
@@ -13,8 +14,10 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:id])
+    @book = Book.find(params[:book_id])
+    BookComment.find(params[:id]).destroy
+
+    # redirect_to book_path(params[:id])
   end
   
     private
